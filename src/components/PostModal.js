d1df678a0367bf122a7fb856c5023eb46f1c29a3
Allam Vachaspati <allamvachaspati05@gmail.com>
2022-01-05 20:12:19 +0530

    user name and image in post modal

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import ReactPlayer from "react-player";
+import { connect } from "react-redux";
 
 const PostModal = (props) => {
     const [editorText, setEditorText] = useState("");
@@ -46,8 +47,12 @@ const PostModal = (props) => {
                         </Header>
                         <SharedContent>
                             <UserInfo>
-                                <img src="/images/user.svg" alt="" />
-                                <span>Name</span>
+                                {props.user.photoURL ? (
+                                    <img src={props.user.photoURL} alt="" />
+                                ) : (
+                                    <img src="/images/user.svg" alt="" />
+                                )}
+                                <span>{props.user.displayName}</span>
                             </UserInfo>
                             <Editor>
                                 <textarea
@@ -259,4 +264,12 @@ const UploadImage = styled.div`
     }
 `;
 
-export default PostModal;
+const mapStateToProps = (state) => {
+    return {
+        user : state.userState.user,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps,mapDispatchToProps)(PostModal);
